031de74aea2af0294b17c780b235594cadd03723
MahdiSohaily <mahdi.sohaily4030@gmail.com>
2022-10-26 15:20:28 +0430

    Add Clock component class

---
+++ src/components/Clock.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+class Clock extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { date: new Date() };
+  }
+
+  
+}
+
+export default Clock;

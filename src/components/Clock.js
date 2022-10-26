b109bdf1b9bb8552e6197c7a6153184130bd9cd2
MahdiSohaily <mahdi.sohaily4030@gmail.com>
2022-10-26 15:35:39 +0430

    Add clock component lifecycle methods

--- src/components/Clock.js
+++ src/components/Clock.js
@@ -6,6 +6,18 @@ class Clock extends React.Component {
     this.state = { date: new Date() };
   }
 
+  componentDidMount() {
+    this.timerID = setInterval(() => this.tick(), 1000);
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.timerID);
+  }
+
+  tick() {
+    this.setState({ date: new Date() });
+  }
+
   render() {
     return (
       <div>

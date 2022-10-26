04ed05d3fdb97b7e3ce58f6aa7610fbf561d750d
MahdiSohaily <mahdi.sohaily4030@gmail.com>
2022-10-26 15:20:50 +0430

    Add component render method

--- src/components/Clock.js
+++ src/components/Clock.js
@@ -6,7 +6,16 @@ class Clock extends React.Component {
     this.state = { date: new Date() };
   }
 
-  
+  render() {
+    return (
+      <div>
+        <div>
+          <h1>Hello, world!</h1>
+          <h2>It is {this.state.date.toLocaleTimeString()}.</h2>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default Clock;

175fc5c590ea320d130de5f37c752ba6b48c4ffb
MahdiSohaily <mahdi.sohaily4030@gmail.com>
2022-10-29 16:09:41 +0430

    Add state to check conditional rendering

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
 import './styles/App.css';
 import Clock from './components/Clock';
 import EventHandler from './components/EventHandler';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isLogin: false,
+    };
+  }
+
   render() {
     return (
       <>

5fac7545982100e125757d63e2cb660ef3ed8cfe
MahdiSohaily <mahdi.sohaily4030@gmail.com>
2022-10-30 22:27:39 +0430

    Add comment ot the code

--- src/pages/contact.jsx
+++ src/pages/contact.jsx
@@ -3,7 +3,10 @@ import React from 'react';
 export default function Contact() {
   const address = ['afghanistan', 'kabul', 'brchi', 'shahrak-e-jafariea'];
   const display = address.map((item, index) => (
-    <li key={index.toString()}>item</li>
+    //we have to pass unique key values while rendering items of an array
+    // the key value should be a unique string value among this array block
+    // so we can the same key value in different array blocks
+    <li key={index.toString()}>{item}</li>
   ));
   return (
     <React.Fragment>

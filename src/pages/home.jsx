9b7c416bc7ff0f79c780a1df85a8e962018c373d
MahdiSohaily <mahdi.sohaily4030@gmail.com>
2022-10-30 23:36:45 +0430

    practice multi input forms

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,5 +1,48 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-export default function Home() {
-  return <div>home</div>;
+export default class Home extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { name: '', lastName: '' };
+  }
+
+  handleInputChange = (event) => {
+    const target = event.target;
+    const value = target.type === 'checkbox' ? target.checked : target.value;
+    const name = target.name;
+
+    this.setState({
+      [name]: value,
+    });
+  };
+
+  componentDidUpdate() {
+    console.log(this.state);
+  }
+
+  render() {
+    return (
+      <form>
+        <label>
+          Is going:
+          <input
+            name="name"
+            type="text"
+            value={this.state.name}
+            onChange={this.handleInputChange}
+          />
+        </label>
+        <br />
+        <label>
+          Number of guests:
+          <input
+            name="lastName"
+            type="text"
+            value={this.state.lastName}
+            onChange={this.handleInputChange}
+          />
+        </label>
+      </form>
+    );
+  }
 }

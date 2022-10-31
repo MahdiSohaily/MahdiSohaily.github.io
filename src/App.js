f145b3c732e9a8bd7fccd3d99d7ba4b8d4282f8e
MahdiSohaily <mahdi.sohaily4030@gmail.com>
2022-10-31 23:45:46 +0430

    Update APP file

--- src/App.js
+++ src/App.js
@@ -1,10 +1,4 @@
 import React, { Component } from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Home from './pages/Home';
-import About from './pages/About';
-import Contact from './pages/Contact';
-import NoPage from './pages/NoPage';
-import Layout from './pages/Layout';
 import './styles/App.css';
 
 class App extends Component {

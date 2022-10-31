2577ffee7e5fe983bf60429770da4330b32c865c
MahdiSohaily <mahdi.sohaily4030@gmail.com>
2022-10-31 23:43:32 +0430

    Removed Unwanted file

--- src/components/Button.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export default function Button(props) {
-  return (
-    <button type="button" onClick={props.handleState}>
-      {props.title}
-    </button>
-  );
-}

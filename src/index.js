d71741997981752d3391980585cb308c4ebdc103
MahdiSohaily <mahdi.sohaily4030@gmail.com>
2022-11-05 22:20:09 +0430

    Add toast state variables

--- src/index.js
+++ src/index.js
@@ -1,13 +1,18 @@
+import { useState } from 'react';
 import ReactDOM from 'react-dom/client';
 import Loading from './components/Loading/';
 import Toast from './components/Toast/';
 import './styles/index.css';
 
 export default function App() {
+  const [toast, setToast] = useState({
+    typeof: 'info',
+    message: 'new Message',
+  });
   return (
     <div>
       <Loading />
-      <Toast type='success' message='done' />
+      <Toast type={toast.typeof} message={toast.message} />
     </div>
   );
 }

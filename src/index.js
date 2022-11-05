f715b149776083b76e3ec7fa779c4a5a2d993f6d
MahdiSohaily <mahdi.sohaily4030@gmail.com>
2022-11-05 21:43:35 +0430

    Rebuild project

--- src/index.js
+++ src/index.js
@@ -1,12 +1,8 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './styles/index.css';
-import App from './App';
+import ReactDOM from "react-dom/client";
+
+export default function App() {
+  return ( <h1>Hello world</h1>);
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
-
+root.render(<App />);

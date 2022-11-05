606fbf774aa2dee33d635ce73276f8564c472705
MahdiSohaily <mahdi.sohaily4030@gmail.com>
2022-11-05 22:16:30 +0430

    Add toast props

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 import ReactDOM from 'react-dom/client';
 import Loading from './components/Loading/';
+import Toast from './components/Toast/';
 import './styles/index.css';
 
 export default function App() {
   return (
     <div>
       <Loading />
+      <Toast type='success' message='done' />
     </div>
   );
 }

f6223fc560fe2329fabbcb033d716833287f0b25
MahdiSohaily <mahdi.sohaily4030@gmail.com>
2022-11-05 22:32:34 +0430

    Add form to get new data

--- src/index.js
+++ src/index.js
@@ -11,19 +11,34 @@ export default function App() {
   });
 
   const [post, setPost] = useState('');
-  const [loading, setLoading] = useState(false);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     fetch('https://jsonplaceholder.typicode.com/posts/1')
       .then((response) => response.json())
       .then((post) => {
         setPost(post.title);
+        setToast({ type: 'success', message: 'Data received successfully' });
+        setLoading(false);
       });
   });
   return (
     <div>
       <div>
         <h1> {post}</h1>
+      </div>
+      <div>
+        <label for>
+          Post ID:
+          <input
+            type="test"
+            name="post_is"
+            value={post}
+            onChange={(e) => {
+              setPost(e.target.value);
+            }}
+          />
+        </label>
       </div>
       {loading && <Loading />}
       <Toast type={toast.typeof} message={toast.message} />

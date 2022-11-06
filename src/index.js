0b1feca8b4444988f0136a2c764e397d2987a4ae
MahdiSohaily <mahdi.sohaily4030@gmail.com>
2022-11-06 09:03:03 +0430

    Implement useReducer

--- src/index.js
+++ src/index.js
@@ -5,43 +5,49 @@ import Toast from './components/Toast/';
 import './styles/index.css';
 
 const initState = {
-  tost : {type : 'info', message : ''},
+  tost: { type: 'info', message: '' },
   post: '',
-  id:1,
+  id: 1,
   loading: true,
+};
+
+function reducer(state, action) {
+  switch (action.type) {
+    case 'get-data':
+      return {
+        ...state,
+        title: action.title,
+        loading: false,
+        tost: {
+          type: 'success',
+          message: `${action.id}-Data received successfully`,
+        },
+      };
+    case 'set_Id':
+      return {
+        ...state,
+        id: action.id,
+        loading: true,
+      };
+    default:
+  }
 }
 
 export default function App() {
-  function userAction(type, payload) {
-    switch (type) {
-      case 'get-data':
-        console.log(payload);
-        setPost(payload.title);
-        setLoading(false);
-        setToast({
-          type: 'success',
-          message: `${payload.id}-Data received successfully`,
-        });
-        break;
-      case 'set_Id':
-        setID(payload);
-        setLoading(true);
-        break;
-      default:
-    }
-  }
-
-
+  const [{ tost, id, loading, post }, dispatch] = useReducer(
+    reducer,
+    initState
+  );
   useEffect(() => {
     fetch(`https://jsonplaceholder.typicode.com/posts/${id}`)
       .then((response) => response.json())
       .then((post) => {
-        userAction('get-data', { title: post.title, id: post.id });
+        dispatch({ type: 'get-data', title: post.title, id: post.id });
       });
   }, [id]);
 
   function handlePostId(e) {
-    userAction('set_Id',e.target.value);
+    dispatch({ type: 'set_Id', id: e.target.value });
   }
 
   return (
@@ -63,7 +69,7 @@ export default function App() {
         </label>
       </div>
       {loading && <Loading />}
-      <Toast type={toast.typeof} message={toast.message} />
+      <Toast type={tost.type} message={tost.message} />
     </div>
   );
 }

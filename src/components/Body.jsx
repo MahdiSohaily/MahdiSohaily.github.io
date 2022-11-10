ef3b0604f1634b2190e9e27f05380a003968fd55
MahdiSohaily <mahdi.sohaily4030@gmail.com>
2022-11-10 10:03:54 +0430

    Update chat component

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -2,12 +2,9 @@ import React from 'react';
 import Chat from './Chat';
 
 export default function Body(props) {
-  const { sender, receiver } = props.users;
   const chats = props.chatList.map((item, index) => {
     console.log(item.type);
-    return (
-        <Chat key={index} />
-    );
+    return <Chat key={index} chat={item} users={props.users} />;
   });
   return (
     <div className="panel-body">
--- src/components/Chat.jsx
+++ src/components/Chat.jsx
@@ -1,31 +1,31 @@
-import React from 'react'
+import React from 'react';
 
-export default function Chat() {
+export default function Chat(props) {
+  const { sender, receiver } = props.users;
+  const { type, message, time } = props.chat;
   return (
-    <div className={`chat ${item.type === 'receive' && 'chat-left'}`}>
-    <div
-      className='chat-avatar'
-    >
-      <a
-        className="avatar avatar-online"
-        data-toggle="tooltip"
-        href="#top"
-        data-placement="right"
-        title=""
-        data-original-title="June Lane"
-      >
-        <img src={item.type === 'receive' ? receiver : sender} alt="..." />
-        <i></i>
-      </a>
-    </div>
-    <div className="chat-body">
-      <div className="chat-content">
-        <p>{item.message}</p>
-        <time className="chat-time" dateTime="2015-07-01T11:37">
-          {item.time}
-        </time>
+    <div className={`chat ${type === 'receive' && 'chat-left'}`}>
+      <div className="chat-avatar">
+        <a
+          className="avatar avatar-online"
+          data-toggle="tooltip"
+          href="#top"
+          data-placement="right"
+          title=""
+          data-original-title="June Lane"
+        >
+          <img src={type === 'receive' ? receiver : sender} alt="..." />
+          <i></i>
+        </a>
+      </div>
+      <div className="chat-body">
+        <div className="chat-content">
+          <p>{message}</p>
+          <time className="chat-time" dateTime="2015-07-01T11:37">
+            {time}
+          </time>
+        </div>
       </div>
     </div>
-  </div>
-  )
+  );
 }

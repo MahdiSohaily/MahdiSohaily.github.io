5ed7223bce831a4ff862843112a84d7ec72d3667
MahdiSohaily <mahdi.sohaily4030@gmail.com>
2022-11-10 08:39:41 +0430

    Removed unwanted filles

--- src/components/Box.jsx
+++
@@ -1,7 +0,0 @@
-import React, { Component } from 'react';
-
-export default class Box extends Component {
-  render() {
-    return <div className="box">{this.props.title}</div>;
-  }
-}
--- src/index.js
+++ src/index.js
@@ -1,13 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import Main from './components/Main';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Main />
   </React.StrictMode>
 );
 

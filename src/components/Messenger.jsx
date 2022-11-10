b7600abd401979ee7d83299737cd224415e0ff12
MahdiSohaily <mahdi.sohaily4030@gmail.com>
2022-11-10 09:20:03 +0430

    Add chat List

--- src/components/Messenger.jsx
+++ src/components/Messenger.jsx
@@ -4,12 +4,31 @@ import Footer from './Footer';
 import Heading from './Heading';
 
 export default class Messenger extends Component {
+  state = {
+    chatList: [
+      {
+        message: 'Good morning, sir What can I do for you?',
+        type: 'send',
+        time: '11:37:08 am',
+      },
+      {
+        message: 'Well, I am just looking around.',
+        type: 'receive',
+        time: '11:39:57 am',
+      },
+      {
+        message: 'If necessary, please ask me.',
+        type: 'send',
+        time: '11:40:10 am',
+      },
+    ],
+  };
   render() {
     return (
       <div class="container bootstrap snippets">
         <div class="col-md-7 col-xs-12 col-md-offset-2">
           <div class="panel" id="chat">
-           <Heading />
+            <Heading />
             <Body />
             <Footer />
           </div>

63fefb92dd27977e09d874835906d538543733e5
MahdiSohaily <mahdi.sohaily4030@gmail.com>
2023-05-21 16:27:07 +0330

    Add component template

---
+++ src/components/Contact/Contact.jsx
@@ -0,0 +1,23 @@
+export default function Contact() {
+  return (
+    <section id="contact" className="mini-section">
+      <h2 className="number contact-heading text-green fs-500 ff-mono align text-center mb-200">
+        Contact
+      </h2>
+      <h2 className="heading-secondary">Lets’s Get In Touch</h2>
+      <p className="text-light-slate text-center mb-300">
+        Although I’m not currently looking for any new opportunities, my inbox
+        is always open. Whether you have a question or just want to say hi, I’ll
+        try my best to get back to you!
+      </p>
+      <a
+        className="resume"
+        href="mailto:brittany.chiang@gmail.com"
+        rel="noopener noreferrer"
+        target="_blank"
+      >
+        Say Hello
+      </a>
+    </section>
+  );
+}

f410d3ed40bbf75bc06de33c9adad13d465ef440
MahdiSohaily <mahdi.sohaily4030@gmail.com>
2023-05-21 16:41:46 +0330

    Add main component template

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,4 +1,9 @@
-// eslint-disable-next-line react/prop-types
+import PropTypes  from 'prop-types';
+
 export default function Main({ children }) {
   return <main>{children}</main>;
 }
+
+Main.propTypes = {
+  children: PropTypes.element.isRequired,
+};

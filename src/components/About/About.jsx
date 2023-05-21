5f998a7c8e0e5ced132cf16aa4bf0062fc163747
MahdiSohaily <mahdi.sohaily4030@gmail.com>
2023-05-21 20:20:16 +0330

    Add social account list

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,4 +1,22 @@
-export default function About() {
+/* eslint-disable quotes */
+/* eslint-disable operator-linebreak */
+/* eslint-disable no-unused-vars */
+/* eslint-disable react/no-array-index-key */
+/* eslint-disable array-callback-return */
+/* eslint-disable consistent-return */
+/* eslint-disable react/prop-types */
+// eslint-disable-next-line no-unused-vars
+export default function About({ person, strengths }) {
+  const level = ["master"];
+  const skills = strengths.map((item) => {
+    if (level.includes(item.proficiency)) {
+      return item.name;
+    }
+  });
+
+  const display =
+    skills && skills.map((skill, index) => <li key={index}>{skill}</li>);
+
   return (
     <section id="about">
       <h2 className="number section-heading text-lightest-slate mb-200">
@@ -7,46 +25,16 @@ export default function About() {
       <div className="inner">
         <div className="bio">
           <div>
-            <p className="text-slate fs-500">
-              Hi! My name is Mahdi Rezaei, and I&#x27;m a full-stack software
-              developer.\n\nI specialize in using TypeScript, React, Redux, and
-              Laravel to build software solutions for small businesses.\n\nI
-              have worked as a software developer for one year in the Ministry
-              of Energy and Water. There I have been working with other groups
-              of software developers in a stressful, fast-paced environment, so
-              I have already learned how to manage stressful situations, remain
-              calm, work collaboratively and behave professionally in a
-              group.\n\nI am currently completing the Microverse Boot Camp, a
-              remote software development Bootcamp that allows programmers to
-              plan and build projects together.\n\n\nIn my free time, I love
-              spending time with my family and friends &amp; reading books. I
-              love running, and video games!\n\nFront-End: JavaScript,
-              TypeScript, React.js, Redux, Vue.js, Next.js.\nBack-End: Laravel,
-              Spring Boot.\nDatabases: MongoDB, MySQL.\nTools and Methods: Git,
-              GitHub, Netlify, Webpack, TDD, RSpec.\n\nLooking for a developer?
-              Let&#x27;s schedule a zoom call! You can send me an e-mail at
-              mahdi.sohaily4030@gmail.com, or check out my profile at
-              https://github.com/MahdiSohaily.
-            </p>
+            <p className="text-slate fs-500">{person.summaryOfBio}</p>
 
             <p className="text-slate fs-500s">
               Here are a few technologies I’ve been working with recently:
             </p>
           </div>
-          <ul className="skills-list text-slate fs-500s">
-            <li>JavaScript (ES6+)</li>
-            <li>TypeScript</li>
-            <li>React</li>
-            <li>Eleventy</li>
-            <li>Node.js</li>
-            <li>WordPress</li>
-          </ul>
+          <ul className="skills-list text-slate fs-500s">{display}</ul>
         </div>
         <div className="profile">
-          <img
-            src="https://res.cloudinary.com/torre-technologies-co/image/upload/v1683013939/origin/starrgate/users/profile_432934781b38b796e1e3f21087f377752cdba2c0.jpg"
-            alt=""
-          />
+          <img src={person.picture} alt={person.name} />
         </div>
       </div>
     </section>

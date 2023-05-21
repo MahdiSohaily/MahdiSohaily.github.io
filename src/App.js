2fdf564013dfea18b4081f0787d323b37362c78e
MahdiSohaily <mahdi.sohaily4030@gmail.com>
2023-05-21 16:58:20 +0330

    Update style file structure

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
-import Main from "./components/Main";
-import Header from "./components/Header/Header";
-import Hero from "./components/Hero/Hero";
-import About from "./components/About/About";
-import Experience from "./components/Experience/Experience";
-import Projects from "./components/Projects/Projects";
-import Contact from "./components/Contact/Contact";
-import "./style.css";
+import Main from './components/Main';
+import Header from './components/Header/Header';
+import Hero from './components/Hero/Hero';
+import About from './components/About/About';
+import Experience from './components/Experience/Experience';
+import Projects from './components/Projects/Projects';
+import Contact from './components/Contact/Contact';
+import './templates/root.css';
+import './templates/reset.css';
+import './templates/utility.css';
+import './style.css';
 
 function App() {
   return (

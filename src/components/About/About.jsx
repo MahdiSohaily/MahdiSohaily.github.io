a8540b456364a2f85b048cfb383c4b9a4ed2be75
MahdiSohaily <mahdi.sohaily4030@gmail.com>
2023-05-21 16:23:02 +0330

    Add About section

---
+++ src/components/About/About.jsx
@@ -0,0 +1,54 @@
+export default function About() {
+  return (
+    <section id="about">
+      <h2 className="number section-heading text-lightest-slate mb-200">
+        About Me
+      </h2>
+      <div className="inner">
+        <div className="bio">
+          <div>
+            <p className="text-slate fs-500">
+              Hi! My name is Mahdi Rezaei, and I&#x27;m a full-stack software
+              developer.\n\nI specialize in using TypeScript, React, Redux, and
+              Laravel to build software solutions for small businesses.\n\nI
+              have worked as a software developer for one year in the Ministry
+              of Energy and Water. There I have been working with other groups
+              of software developers in a stressful, fast-paced environment, so
+              I have already learned how to manage stressful situations, remain
+              calm, work collaboratively and behave professionally in a
+              group.\n\nI am currently completing the Microverse Boot Camp, a
+              remote software development Bootcamp that allows programmers to
+              plan and build projects together.\n\n\nIn my free time, I love
+              spending time with my family and friends &amp; reading books. I
+              love running, and video games!\n\nFront-End: JavaScript,
+              TypeScript, React.js, Redux, Vue.js, Next.js.\nBack-End: Laravel,
+              Spring Boot.\nDatabases: MongoDB, MySQL.\nTools and Methods: Git,
+              GitHub, Netlify, Webpack, TDD, RSpec.\n\nLooking for a developer?
+              Let&#x27;s schedule a zoom call! You can send me an e-mail at
+              mahdi.sohaily4030@gmail.com, or check out my profile at
+              https://github.com/MahdiSohaily.
+            </p>
+
+            <p className="text-slate fs-500s">
+              Here are a few technologies I’ve been working with recently:
+            </p>
+          </div>
+          <ul className="skills-list text-slate fs-500s">
+            <li>JavaScript (ES6+)</li>
+            <li>TypeScript</li>
+            <li>React</li>
+            <li>Eleventy</li>
+            <li>Node.js</li>
+            <li>WordPress</li>
+          </ul>
+        </div>
+        <div className="profile">
+          <img
+            src="https://res.cloudinary.com/torre-technologies-co/image/upload/v1683013939/origin/starrgate/users/profile_432934781b38b796e1e3f21087f377752cdba2c0.jpg"
+            alt=""
+          />
+        </div>
+      </div>
+    </section>
+  );
+}

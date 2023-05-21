206e0ba92eda88773f3de579e46af3212436e662
MahdiSohaily <mahdi.sohaily4030@gmail.com>
2023-05-21 20:35:50 +0330

    Add social account list

--- src/components/Experience/Experience.jsx
+++ src/components/Experience/Experience.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable no-restricted-syntax */
 /* eslint-disable quotes */
 /* eslint-disable react/prop-types */
 /* eslint-disable no-unused-vars */
@@ -8,6 +9,17 @@ import "./experience.css";
 
 export default function Experience({ jobs }) {
   const [active, setActive] = useState(null);
+
+  const tabs = [];
+  const selectedJobs = jobs.length >= 4 ? jobs.slice(0, 5) : jobs;
+
+  for (const item of selectedJobs) {
+    tabs[item.id] = <Details key={item.id} content={item.additionalInfo} />;
+  }
+
+  const handleActive = (item) => {
+    setActive(item);
+  };
   return (
     <section id="jobs" className="mini-section">
       <h2 className="number section-heading text-lightest-slate mb-200">

a22c23bdcc605228a64591216b47a4826fbd2710
MahdiSohaily <mahdi.sohaily4030@gmail.com>
2023-05-21 16:25:46 +0330

    Add projects component

---
+++ src/components/Projects/Projects.jsx
@@ -0,0 +1,323 @@
+export default function Projects() {
+  return (
+    <section id="projects">
+      <h2 className="number section-heading text-lightest-slate mb-200">
+        Other Noteworthy Projects
+      </h2>
+      <div className="projects-grid">
+        <div className="project-card">
+          <div className="card-heading mb-200">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              role="img"
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="currentColor"
+              strokeWidth="1"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              className="folder-icon feather feather-folder"
+            >
+              <title>Folder</title>
+              <path d="M22 19a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h5l2 3h9a2 2 0 0 1 2 2z" />
+            </svg>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              role="img"
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="currentColor"
+              strokeWidth="2"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              className="feather feather-external-link"
+            >
+              <title>External Link</title>
+              <path d="M18 13v6a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V8a2 2 0 0 1 2-2h6" />
+              <polyline points="15 3 21 3 21 9" />
+              <line x1="10" y1="14" x2="21" y2="3" />
+            </svg>
+          </div>
+          <div className="card-body">
+            <h3 className="project-title">
+              Integrating Algolia Search with WordPress Multisite
+            </h3>
+            <div className="project-description">
+              <p className="text-light-slate fs-md">
+                Building a custom multisite compatible WordPress plugin to build
+                global search with Algolia
+              </p>
+            </div>
+          </div>
+          <footer>
+            <ul className="project-tech-list">
+              <li>Algolia</li>
+              <li>WordPress</li>
+              <li>PHP</li>
+            </ul>
+          </footer>
+        </div>
+        <div className="project-card">
+          <div className="card-heading mb-200">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              role="img"
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="currentColor"
+              strokeWidth="1"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              className="folder-icon"
+            >
+              <title>Folder</title>
+              <path d="M22 19a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h5l2 3h9a2 2 0 0 1 2 2z" />
+            </svg>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              role="img"
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="currentColor"
+              strokeWidth="2"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              className="feather feather-external-link"
+            >
+              <title>External Link</title>
+              <path d="M18 13v6a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V8a2 2 0 0 1 2-2h6" />
+              <polyline points="15 3 21 3 21 9" />
+              <line x1="10" y1="14" x2="21" y2="3" />
+            </svg>
+          </div>
+          <div className="card-body">
+            <h3 className="project-title">
+              Integrating Algolia Search with WordPress Multisite
+            </h3>
+            <div className="project-description">
+              <p className="text-light-slate fs-md">
+                Building a custom multisite compatible WordPress plugin to build
+                global search with Algolia
+              </p>
+            </div>
+          </div>
+          <footer>
+            <ul className="project-tech-list">
+              <li>Algolia</li>
+              <li>WordPress</li>
+              <li>PHP</li>
+            </ul>
+          </footer>
+        </div>
+        <div className="project-card">
+          <div className="card-heading mb-200">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              role="img"
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="currentColor"
+              strokeWidth="1"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              className="folder-icon"
+            >
+              <title>Folder</title>
+              <path d="M22 19a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h5l2 3h9a2 2 0 0 1 2 2z" />
+            </svg>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              role="img"
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="currentColor"
+              strokeWidth="2"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              className="feather feather-external-link"
+            >
+              <title>External Link</title>
+              <path d="M18 13v6a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V8a2 2 0 0 1 2-2h6" />
+              <polyline points="15 3 21 3 21 9" />
+              <line x1="10" y1="14" x2="21" y2="3" />
+            </svg>
+          </div>
+          <div className="card-body">
+            <h3 className="project-title">
+              Integrating Algolia Search with WordPress Multisite
+            </h3>
+            <div className="project-description">
+              <p className="text-light-slate fs-md">
+                Building a custom multisite compatible WordPress plugin to build
+                global search with Algolia
+              </p>
+            </div>
+          </div>
+          <footer>
+            <ul className="project-tech-list">
+              <li>Algolia</li>
+              <li>WordPress</li>
+              <li>PHP</li>
+            </ul>
+          </footer>
+        </div>
+        <div className="project-card">
+          <div className="card-heading mb-200">
+            <svg
+              className="folder-icon"
+              xmlns="http://www.w3.org/2000/svg"
+              role="img"
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="currentColor"
+              strokeWidth="1"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+            >
+              <title>Folder</title>
+              <path d="M22 19a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h5l2 3h9a2 2 0 0 1 2 2z" />
+            </svg>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              role="img"
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="currentColor"
+              strokeWidth="2"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              className="feather feather-external-link"
+            >
+              <title>External Link</title>
+              <path d="M18 13v6a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V8a2 2 0 0 1 2-2h6" />
+              <polyline points="15 3 21 3 21 9" />
+              <line x1="10" y1="14" x2="21" y2="3" />
+            </svg>
+          </div>
+          <div className="card-body">
+            <h3 className="project-title">
+              Integrating Algolia Search with WordPress Multisite
+            </h3>
+            <div className="project-description">
+              <p className="text-light-slate fs-md">
+                Building a custom multisite compatible WordPress plugin to build
+                global search with Algolia
+              </p>
+            </div>
+          </div>
+          <footer>
+            <ul className="project-tech-list">
+              <li>Algolia</li>
+              <li>WordPress</li>
+              <li>PHP</li>
+            </ul>
+          </footer>
+        </div>
+        <div className="project-card">
+          <div className="card-heading mb-200">
+            <svg
+              className="folder-icon"
+              xmlns="http://www.w3.org/2000/svg"
+              role="img"
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="currentColor"
+              strokeWidth="1"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+            >
+              <title>Folder</title>
+              <path d="M22 19a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h5l2 3h9a2 2 0 0 1 2 2z" />
+            </svg>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              role="img"
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="currentColor"
+              strokeWidth="2"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              className="feather feather-external-link"
+            >
+              <title>External Link</title>
+              <path d="M18 13v6a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V8a2 2 0 0 1 2-2h6" />
+              <polyline points="15 3 21 3 21 9" />
+              <line x1="10" y1="14" x2="21" y2="3" />
+            </svg>
+          </div>
+          <div className="card-body">
+            <h3 className="project-title">
+              Integrating Algolia Search with WordPress Multisite
+            </h3>
+            <div className="project-description">
+              <p className="text-light-slate fs-md">
+                Building a custom multisite compatible WordPress plugin to build
+                global search with Algolia
+              </p>
+            </div>
+          </div>
+          <footer>
+            <ul className="project-tech-list">
+              <li>Algolia</li>
+              <li>WordPress</li>
+              <li>PHP</li>
+            </ul>
+          </footer>
+        </div>
+        <div className="project-card">
+          <div className="card-heading mb-200">
+            <svg
+              className="folder-icon"
+              xmlns="http://www.w3.org/2000/svg"
+              role="img"
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="currentColor"
+              strokeWidth="1"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+            >
+              <title>Folder</title>
+              <path d="M22 19a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h5l2 3h9a2 2 0 0 1 2 2z" />
+            </svg>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              role="img"
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="currentColor"
+              strokeWidth="2"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              className="feather feather-external-link"
+            >
+              <title>External Link</title>
+              <path d="M18 13v6a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V8a2 2 0 0 1 2-2h6" />
+              <polyline points="15 3 21 3 21 9" />
+              <line x1="10" y1="14" x2="21" y2="3" />
+            </svg>
+          </div>
+          <div className="card-body">
+            <h3 className="project-title">
+              Integrating Algolia Search with WordPress Multisite
+            </h3>
+            <div className="project-description">
+              <p className="text-light-slate fs-md">
+                Building a custom multisite compatible WordPress plugin to build
+                global search with Algolia
+              </p>
+            </div>
+          </div>
+          <footer>
+            <ul className="project-tech-list">
+              <li>Algolia</li>
+              <li>WordPress</li>
+              <li>PHP</li>
+            </ul>
+          </footer>
+        </div>
+      </div>
+    </section>
+  );
+}

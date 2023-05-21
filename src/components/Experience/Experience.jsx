60f3351f3483fca38a7dec06d4108991f732ca57
MahdiSohaily <mahdi.sohaily4030@gmail.com>
2023-05-21 20:25:28 +0330

    Add social account list

--- src/components/Experience/Experience.jsx
+++ src/components/Experience/Experience.jsx
@@ -1,8 +1,13 @@
-import Details from './Details';
-import Tab from './Tab';
-import './experience.css';
+/* eslint-disable quotes */
+/* eslint-disable react/prop-types */
+/* eslint-disable no-unused-vars */
+import { useState } from "react";
+import Details from "./Details";
+import Tab from "./Tab";
+import "./experience.css";
 
-export default function Experience() {
+export default function Experience({ jobs }) {
+  const [active, setActive] = useState(null);
   return (
     <section id="jobs" className="mini-section">
       <h2 className="number section-heading text-lightest-slate mb-200">

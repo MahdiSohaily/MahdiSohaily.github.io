f271e33c77832ad294e710ea00e45ba3f2d30752
MahdiSohaily <mahdi.sohaily4030@gmail.com>
2023-05-21 23:02:30 +0330

    Add social account list

--- src/components/Experience/Tab.jsx
+++ src/components/Experience/Tab.jsx
@@ -1,8 +1,3 @@
-/* eslint-disable react/prop-types */
-/* eslint-disable jsx-a11y/click-events-have-key-events */
-/* eslint-disable jsx-a11y/no-static-element-interactions */
-import PropTypes from 'prop-types';
-
 export default function Tab({
   id = null, title, handleActive, active,
 }) {
@@ -18,8 +13,3 @@ export default function Tab({
   );
 }
 
-Tab.propTypes = {
-  title: PropTypes.string.isRequired,
-  active: PropTypes.string.isRequired,
-  handleActive: PropTypes.string.isRequired,
-};
